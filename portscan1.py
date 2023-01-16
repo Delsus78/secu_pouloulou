e174cca5ac2ed7a901f75e6247d63f01c26814e2
Delsus78 <delsustheugm@gmail.com>
2023-01-16 10:28:48 +0100

    q6 et 7 fix

--- portscan1.py
+++ portscan1.py
@@ -7,8 +7,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "127.0.0.1"
 port = 443
 
+
 def portscanner(port):
     if s.connect_ex((host, port)):
         print("Port %d is closed" % (port))
     else:
         print("Port %d is open" % (port))
+
+
+portscanner(port)
