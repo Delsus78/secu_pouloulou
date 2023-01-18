aeda41727eca057bc140be75346183ee9df197fe
Delsus78 <delsustheugm@gmail.com>
2023-01-18 10:42:32 +0100

    omg y'a moyen que ca soit ouf v6

--- portscanneradv.py
+++ portscanneradv.py
@@ -8,6 +8,8 @@ import chardet
 
 
 def main():
+    socket.setdefaulttimeout(2)
+
     parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
     parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
     parser.add_option('-p', dest='tgtPort', type='string', help='specify target port[s] separated by comma')
@@ -40,19 +42,23 @@ def portScan(host, ports):
 def portscanner(host, port):
     print(f'Scanning port {port}')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if s.connect_ex((host, port)):
-        # print in color red
-        print(f"\033[1;31;40mPort {port} is closed \033[0;37;40m")
-    else:
+    try:
+        s.connect((host, port))
         # print in color green
         print(f"\033[1;32;40mPort {port} is open : \nBanner : {retBanner(s)} \033[0;37;40m")
+
+    except:
+        # print in color red
+        print(f"\033[1;31;40mPort {port} is closed \033[0;37;40m")
 
 
 # Améliorer votre programme, en ajoutant la fonction retBanner() qui permet d'afficher la version des services qui utilisent les ports ouverts (banners). Utilisez la méthode recv() de la bibliothèque socket
 def retBanner(s):
-    socket.setdefaulttimeout(2)
-    banner = s.recv(1024)
-    return banner
+    try:
+        banner = s.recv(1024)
+        return banner
+    except:
+        return "Error in banner"
 
 
 if __name__ == '__main__':
